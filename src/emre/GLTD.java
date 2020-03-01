3734098a0569a411287177ea444cdc4bd945fc30
Emre Harbutoğlu <emre.harbutoglu@hemenis.com>
2020-03-02 01:53:16 +0300

    improved 3D modelling

--- src/emre/GLTD.java
+++ src/emre/GLTD.java
@@ -9,7 +9,6 @@ package emre;
  *
  * @author emre
  */
-
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
 
@@ -25,6 +24,8 @@ import com.jogamp.opengl.awt.GLCanvas;
 import com.jogamp.opengl.util.Animator;
 import com.jogamp.opengl.util.gl2.GLUT;
 import java.awt.BorderLayout;
+import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
 import javax.swing.JPanel;
 
 public class GLTD extends JPanel implements GLEventListener, MouseMotionListener, OnNewDataReceived {
@@ -41,13 +42,13 @@ public class GLTD extends JPanel implements GLEventListener, MouseMotionListener
         GLCapabilities glCapabilities = new GLCapabilities(glProfile);
         glCapabilities.setDoubleBuffered(true);
         GLCanvas glCanvas = new GLCanvas(glCapabilities);
-        
+
         glCanvas.addGLEventListener(this);
-        
-        //glCanvas.addMouseMotionListener(this);
+
+        glCanvas.addMouseMotionListener(this);
         add(glCanvas);
-        //addMouseMotionListener(this);
-       
+        addMouseMotionListener(this);
+
         rotateX = 0f;
         rotateY = 0f;
         Animator a = new Animator(glCanvas);
@@ -94,7 +95,8 @@ public class GLTD extends JPanel implements GLEventListener, MouseMotionListener
         gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
         gl.glMatrixMode(GL2.GL_PROJECTION);
         gl.glLoadIdentity();
-        gl.glOrtho(-1, 1, -1, 1, -2, 2);
+        gl.glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+
         gl.glMatrixMode(GL2.GL_MODELVIEW);
         gl.glLoadIdentity();
         //gl.glShadeModel(GLLightingFunc.GL_FLAT);
@@ -102,9 +104,17 @@ public class GLTD extends JPanel implements GLEventListener, MouseMotionListener
         gl.glRotatef(rotateX, 0, 1, 0);
         gl.glRotatef(rotateY, 1, 0, 0);
 
-        GLUT glut = new GLUT();
-        glut.glutSolidCylinder(0.1, 0.7, 25, 1000);
-        //glut.glutSolidCone(0.1, 0.7, 25, 500);
+        final GLUT glut = new GLUT();
+
+        for (double i = 0; i < 0.1; i = i + 0.002) {
+            glut.glutSolidCylinder(0.10 - i, 0.8 + i, 9, 50);
+        }
+
+        gl.glClearColor(0, 0, 0, 0);
+        gl.glColor3f(0.63f, 0.63f, 0.63f);
+
+        glut.glutWireCone(0.165, 0.65, 3, 500);
+
     }
 
     @Override
@@ -116,6 +126,9 @@ public class GLTD extends JPanel implements GLEventListener, MouseMotionListener
         gl.glEnable(GL2.GL_LIGHT0);
         gl.glEnable(GL2.GL_COLOR_MATERIAL);
         gl.glEnable(GL2.GL_NORMALIZE);
+
+        rotateX = -25;
+        rotateY = -90;
     }
 
     @Override
@@ -129,22 +142,69 @@ public class GLTD extends JPanel implements GLEventListener, MouseMotionListener
     @Override
     public void mouseMoved(MouseEvent e) {
         lastX = e.getX();
-        lastY = e.getY();   
+        lastY = e.getY();
     }
 
     @Override
     public void mouseDragged(MouseEvent e) {
         rotateX += e.getX() - lastX;
         rotateY += e.getY() - lastY;
-        System.out.println("rotateX"+rotateX);
-        System.out.println("rotateY"+rotateY);     
+        System.out.println("rotateX" + rotateX);
+        System.out.println("rotateY" + rotateY);
         lastX = e.getX();
         lastY = e.getY();
     }
 
     @Override
     public void onNewDataReceived(int[] data) {
+        System.out.println("datas" + data[0] + "," + data[1]);
         rotateX += data[0];
         rotateY += data[1];
     }
+
+    /*
+    
+    private void drawCarets(final GL2 gl, final GLU glu) {
+		final Graphics2D og2d = caretOverlay.createGraphics();
+		setRenderingHints(og2d);
+		
+		og2d.setBackground(new Color(0, 0, 0, 0));
+		og2d.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+		caretOverlay.markDirty(0, 0, canvas.getWidth(), canvas.getHeight());
+		
+		// The existing relative Extras don't really work right for 3d.
+		Coordinate pCP = project(cp, gl, glu);
+		Coordinate pCG = project(cg, gl, glu);
+		
+		final int d = CARET_SIZE / 2;
+		
+		//z order the carets 
+		if (pCG.z < pCP.z) {
+			//Subtract half of the caret size, so they are centered ( The +/- d in each translate)
+			//Flip the sense of the Y coordinate from GL to normal (Y+ up/down)
+			og2d.drawRenderedImage(
+					cpCaretRaster,
+					AffineTransform.getTranslateInstance((pCP.x - d),
+							canvas.getHeight() - (pCP.y + d)));
+			og2d.drawRenderedImage(
+					cgCaretRaster,
+					AffineTransform.getTranslateInstance((pCG.x - d),
+							canvas.getHeight() - (pCG.y + d)));
+		} else {
+			og2d.drawRenderedImage(
+					cgCaretRaster,
+					AffineTransform.getTranslateInstance((pCG.x - d),
+							canvas.getHeight() - (pCG.y + d)));
+			og2d.drawRenderedImage(
+					cpCaretRaster,
+					AffineTransform.getTranslateInstance((pCP.x - d),
+							canvas.getHeight() - (pCP.y + d)));
+		}
+		og2d.dispose();
+		
+		gl.glEnable(GL.GL_BLEND);
+		caretOverlay.drawAll();
+		gl.glDisable(GL.GL_BLEND);
+	}
+     */
 }
